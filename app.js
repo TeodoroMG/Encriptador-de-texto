68f8c1b82293a5570cbae9c918d0696a58f74fdc
TeodoroMG <teodoromatarritagomez@gmail.com>
2024-02-23 11:35:47 -0600

    actualiza la matriz

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const campo_texto = document.querySelector("#texto-encriptado");
 const campo_mensaje = document.querySelector("#campo-mensaje");
 
 
-conts matriz_code = [
+const matriz_code = [
 [ "e", "enter"],//índice 0
 [ "i", "imes"],//índice 1
 [ "a", "ai"],//índice 2

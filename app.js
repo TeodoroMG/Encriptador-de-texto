a06f160d6cf911cd4bae88565adde3c47c331e8c
TeodoroMG <teodoromatarritagomez@gmail.com>
2024-02-23 11:34:06 -0600

    creando la matriz

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
-alert("Hello world");
+
+const campo_texto = document.querySelector("#texto-encriptado");
+const campo_mensaje = document.querySelector("#campo-mensaje");
+
+
+conts matriz_code = [
+[ "e", "enter"],//índice 0
+[ "i", "imes"],//índice 1
+[ "a", "ai"],//índice 2
+[ "o", "ober"],//índice 3
+[ "u", "ufat"],//índice 4
+];
